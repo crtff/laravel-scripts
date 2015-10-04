427ef9acb573308d6bc53e7b15f0db766a515a0d
crtff <1@crtff.com>
2015-10-05 02:18:37 +0300

    Update phpmyadmin.sh
    Fixed serve script path.

--- phpmyadmin.sh
+++ phpmyadmin.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-cd ~/Code
-
 echo 'Downloading PhpMyAdmin 4.5.0.2'
 curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.5.0.2/phpMyAdmin-4.5.0.2-english.tar.gz -o phpmyadmin.tar.gz
 
-echo 'Installing PhpMyAdmin 4.5.0.2 into ~/Code'
+echo 'Installing PhpMyAdmin 4.5.0.2 into '$(pwd)
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 rm phpmyadmin.tar.gz
 
 echo 'Enabling pma.app'
-serve pma.app $(pwd)/phpmyadmin
+sudo bash /vagrant/scripts/serve-hhvm.sh serve pma.app $(pwd)/phpmyadmin
