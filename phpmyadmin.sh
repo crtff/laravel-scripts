39a466e68ebec79f47d1cfa25d7af9e5066be859
crtff <1@crtff.com>
2015-10-05 02:08:55 +0300

    Create phpmyadmin.sh

---
+++ phpmyadmin.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/Code
+
+echo 'Downloading PhpMyAdmin 4.5.0.2'
+curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.5.0.2/phpMyAdmin-4.5.0.2-english.tar.gz -o phpmyadmin.tar.gz
+
+echo 'Installing PhpMyAdmin 4.5.0.2 into ~/Code'
+mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
+rm phpmyadmin.tar.gz
+
+echo 'Enabling pma.app'
+sudo bash /vagrant/scripts/serve.sh pma.app $(pwd)/phpmyadmin
